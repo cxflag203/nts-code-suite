28357b8da8c0f852e9947ad6386c5ed7e4072265
GooD.NTS@gmail.com <GooD.NTS@gmail.com@87510349-76a9-6b36-59c2-4ffb306e77e0>
2011-08-13 13:23:25 +0000

    Update NTSCode lib demo (Add demo for Gradient)

--- trunk/NTS Code Library/Demo/Sources/UIDemoWindow.pas
+++ trunk/NTS Code Library/Demo/Sources/UIDemoWindow.pas
@@ -5,7 +5,7 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, StdCtrls, NTS.Code.Components.WindowConfig,
-  NTS.Code.Components.VirtualStringList;
+  NTS.Code.Components.VirtualStringList, ExtCtrls;
 
 type
   TDemoWindow = class(TForm)
@@ -13,8 +13,13 @@ type
     GroupBox1: TGroupBox;
     CheckBox1: TCheckBox;
     Button1: TButton;
+    PaintBox1: TPaintBox;
+    Label1: TLabel;
+    RadioGroup1: TRadioGroup;
     procedure CheckBox1Click(Sender: TObject);
     procedure Button1Click(Sender: TObject);
+    procedure PaintBox1Paint(Sender: TObject);
+    procedure RadioGroup1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,6 +30,9 @@ var
   DemoWindow: TDemoWindow;
 
 implementation
+
+uses
+  NTS.Code.Graphics.Gradient;
 
 {$R *.dfm}
 
@@ -42,6 +50,17 @@ end;
 procedure TDemoWindow.CheckBox1Click(Sender: TObject);
 begin
   WindowConfig.ShowCaptionBar:= CheckBox1.Checked;
+end;
+
+procedure TDemoWindow.PaintBox1Paint(Sender: TObject);
+begin
+  TGradientClass.Get( TGradientType(RadioGroup1.ItemIndex), PaintBox1.Canvas,
+    PaintBox1.ClientRect, clSkyBlue, clWhite);
+end;
+
+procedure TDemoWindow.RadioGroup1Click(Sender: TObject);
+begin
+  PaintBox1.Invalidate;
 end;
 
 end.
